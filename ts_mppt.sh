2f2580fbe68721825ff4c00e7bb45176ebfe5103
Jean-Marc Lefébure <lefebure.jeanmarc@gmail.com>
2012-02-27 11:27:38 +0300

    run both app for testing purpose

--- ts_mppt.sh
+++ ts_mppt.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/usr/local/bin/ts_mppt | gawk -f /usr/local/bin/ts_mppt.awk >> /var/log/ts_mppt.log
+#could we do the logging from crontab?
+/usr/local/bin/ts_45 | gawk -f /usr/local/bin/ts_mppt.awk >> /var/log/ts_45.log
+/usr/local/bin/ts_mppt_45 | gawk -f /usr/local/bin/ts_mppt.awk >> /var/log/ts_mppt_45.log
